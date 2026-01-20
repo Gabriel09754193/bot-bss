af49e3177f5150d699cdce582bc31bf15bf93081
Gabriel09754193 <105935059+Gabriel09754193@users.noreply.github.com>
2026-01-20 18:33:35 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log("🤖 Bot da liga CS2 online!");
+});
+
+client.login(process.env.TOKEN);

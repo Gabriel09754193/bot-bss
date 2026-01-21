fb8c83fdc006053ce0b5a1e0f199f04d847446d1
Gabriel09754193 <105935059+Gabriel09754193@users.noreply.github.com>
2026-01-21 01:15:15 -0300

    Update times.js

--- comandos/times.js
+++ comandos/times.js
@@ -7,25 +7,17 @@ module.exports = {
   async execute(message) {
     const arquivo = './data/times.json';
     let times = [];
+    if (fs.existsSync(arquivo)) times = JSON.parse(fs.readFileSync(arquivo, 'utf-8'));
 
-    if (fs.existsSync(arquivo)) {
-      times = JSON.parse(fs.readFileSync(arquivo, 'utf-8'));
-    }
-
-    if (times.length === 0) {
-      return message.reply('❌ Nenhum time cadastrado ainda!');
-    }
+    if (times.length === 0) return message.reply('❌ Nenhum time cadastrado ainda!');
 
     const embed = new EmbedBuilder()
       .setTitle('📋 Times Inscritos')
-      .setColor('#00ff99');
-
-    times.forEach((time, i) => {
-      embed.addFields({
-        name: `${i + 1}. ${time.nome} (IGL: <@${time.igl}>)`,
-        value: `🎮 Jogadores:\n${time.jogadores.join('\n')}`
-      });
-    });
+      .setColor('#00ff99')
+      .addFields(times.map((t, i) => ({
+        name: `${i+1}. ${t.nome} (IGL: <@${t.igl}>)`,
+        value: `🎮 Jogadores:\n${t.jogadores.join('\n')}`
+      })));
 
     await message.reply({ embeds: [embed] });
     await message.delete().catch(() => {});

03e3b6958773c59c86efc73bf9445c449a914439
Gabriel09754193 <105935059+Gabriel09754193@users.noreply.github.com>
2026-01-21 11:24:10 -0300

    Update inscricao.js

--- comandos/inscricao.js
+++ comandos/inscricao.js
@@ -1,64 +1,36 @@
-const { SlashCommandBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('inscricao')
-    .setDescription('Cadastrar seu time na competição'),
+  nome: 'inscricao',
+  descricao: 'Cadastrar seu time',
+  async execute(message, args) {
+    const filter = m => m.author.id === message.author.id;
+    const channel = message.channel;
 
-  async execute(interaction) {
-    // Criar modal
-    const modal = new ModalBuilder()
-      .setCustomId('inscricaoModal')
-      .setTitle('Cadastro do Time');
+    await channel.send('Digite o nome do seu time:');
 
-    const nomeTime = new TextInputBuilder()
-      .setCustomId('nomeTime')
-      .setLabel('Nome do Time')
-      .setStyle(TextInputStyle.Short)
-      .setRequired(true);
+    const nomeTime = (await channel.awaitMessages({ filter, max: 1, time: 60000 })).first();
+    if (!nomeTime) return channel.send('❌ Tempo esgotado.');
 
-    const jogadores = new TextInputBuilder()
-      .setCustomId('jogadores')
-      .setLabel('Função e nick dos jogadores (ex: IGL: Player1)')
-      .setStyle(TextInputStyle.Paragraph)
-      .setRequired(true);
+    await channel.send('Digite os jogadores e suas funções (uma linha por jogador):');
+    const jogadoresMsg = (await channel.awaitMessages({ filter, max: 1, time: 120000 })).first();
+    if (!jogadoresMsg) return channel.send('❌ Tempo esgotado.');
 
-    const row1 = new ActionRowBuilder().addComponents(nomeTime);
-    const row2 = new ActionRowBuilder().addComponents(jogadores);
+    const filePath = path.join(__dirname, '../data/times.json');
+    let times = [];
+    if (fs.existsSync(filePath)) {
+      times = JSON.parse(fs.readFileSync(filePath));
+    }
 
-    modal.addComponents(row1, row2);
-
-    await interaction.showModal(modal);
-
-    // Esperar submissão do modal
-    const filter = i => i.customId === 'inscricaoModal' && i.user.id === interaction.user.id;
-    const collector = interaction.channel.createMessageComponentCollector({ filter, time: 5 * 60 * 1000 });
-
-    collector.on('collect', async i => {
-      if (!i.isModalSubmit()) return;
-
-      const nome = i.fields.getTextInputValue('nomeTime');
-      const players = i.fields.getTextInputValue('jogadores');
-
-      // Salvar no times.json
-      const filePath = path.join(__dirname, '../data/times.json');
-      let times = [];
-      if (fs.existsSync(filePath)) {
-        times = JSON.parse(fs.readFileSync(filePath));
-      }
-
-      times.push({
-        id: i.user.id,
-        nome,
-        jogadores: players.split('\n')
-      });
-
-      fs.writeFileSync(filePath, JSON.stringify(times, null, 2));
-
-      // Mensagem de confirmação
-      await i.reply({ content: `✅ Time **${nome}** registrado com sucesso!`, ephemeral: true });
+    times.push({
+      id: message.author.id,
+      nome: nomeTime.content,
+      jogadores: jogadoresMsg.content.split('\n')
     });
+
+    fs.writeFileSync(filePath, JSON.stringify(times, null, 2));
+
+    channel.send(`✅ Time **${nomeTime.content}** registrado com sucesso!`);
   }
 };

e5201fc7815ee27055cfdf4042a950cc7d557196
Gabriel09754193 <105935059+Gabriel09754193@users.noreply.github.com>
2026-01-21 10:12:20 -0300

    Update inscricao.js

--- comandos/inscricao.js
+++ comandos/inscricao.js
@@ -1,16 +1,13 @@
-const fs = require('fs');
-const { ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder, InteractionType } = require('discord.js');
+const { ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } = require('discord.js');
 
 module.exports = {
   name: 'inscricao',
 
-  async execute(message, args, client) {
-    // Criar modal
+  async execute(message, args) {
     const modal = new ModalBuilder()
       .setCustomId('inscricaoModal')
       .setTitle('Cadastro de Time');
 
-    // Campo do nome do time
     const nomeTimeInput = new TextInputBuilder()
       .setCustomId('nomeTime')
       .setLabel('Nome do Time')
@@ -18,7 +15,6 @@ module.exports = {
       .setPlaceholder('Digite o nome do seu time')
       .setRequired(true);
 
-    // Campo dos jogadores
     const jogadoresInput = new TextInputBuilder()
       .setCustomId('jogadores')
       .setLabel('Jogadores (separe por vírgula)')
@@ -26,17 +22,11 @@ module.exports = {
       .setPlaceholder('Ex: Jogador1, Jogador2, Jogador3')
       .setRequired(true);
 
-    // Adicionar campos em action rows
     const firstRow = new ActionRowBuilder().addComponents(nomeTimeInput);
     const secondRow = new ActionRowBuilder().addComponents(jogadoresInput);
+
     modal.addComponents(firstRow, secondRow);
 
-    // Mostrar modal
     await message.showModal(modal);
-
-    // Esperar resposta do modal
-    const filter = i => i.customId === 'inscricaoModal' && i.user.id === message.author.id;
-
-    const collector = message.channel.createMessageComponentCollector({ filter, time: 60000, max: 1 });
-
-    collector.on('collect
+  }
+};

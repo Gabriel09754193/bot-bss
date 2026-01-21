a3306a21a47b0d4c631d7c84ea8896836c24f582
Gabriel09754193 <105935059+Gabriel09754193@users.noreply.github.com>
2026-01-21 00:49:04 -0300

    Update times.js

--- comandos/times.js
+++ comandos/times.js
@@ -1,1 +1,36 @@
+const fs = require('fs');
+const { EmbedBuilder } = require('discord.js');
 
+module.exports = {
+  name: 'times',
+
+  async execute(message) {
+    const arquivo = './data/times.json';
+    let times = [];
+
+    if (fs.existsSync(arquivo)) {
+      times = JSON.parse(fs.readFileSync(arquivo, 'utf-8'));
+    }
+
+    if (times.length === 0) {
+      return message.reply('❌ Nenhum time cadastrado ainda!');
+    }
+
+    const embed = new EmbedBuilder()
+      .setTitle('📋 Times Inscritos')
+      .setColor('#00ff99');
+
+    // Para cada time, adiciona no embed
+    times.forEach((time, i) => {
+      embed.addFields({
+        name: `${i + 1}. ${time.nome} (IGL: <@${time.igl}>)`,
+        value: `🎮 Jogadores:\n${time.jogadores.join('\n')}`
+      });
+    });
+
+    await message.reply({ embeds: [embed] });
+
+    // apaga a mensagem do comando
+    await message.delete().catch(() => {});
+  }
+};

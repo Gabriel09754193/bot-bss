706175af9d57dcc0381bb2c2c0f995d21d0c076d
Gabriel09754193 <105935059+Gabriel09754193@users.noreply.github.com>
2026-01-20 23:49:52 -0300

    Update match.js

--- comandos/match.js
+++ comandos/match.js
@@ -1,11 +1,37 @@
+const {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle
+} = require('discord.js');
+
 module.exports = {
   name: 'match',
 
-  execute(message) {
-    message.reply(
-      '🎮 **MATCH ABERTO!**\n' +
-      'Seu time está disponível para jogar.\n' +
-      'Aguardando adversário.'
+  async execute(message) {
+    const modal = new ModalBuilder()
+      .setCustomId('modal_match')
+      .setTitle('Abrir Match');
+
+    const time = new TextInputBuilder()
+      .setCustomId('time')
+      .setLabel('Nome do seu time')
+      .setStyle(TextInputStyle.Short)
+      .setRequired(true);
+
+    const formato = new TextInputBuilder()
+      .setCustomId('formato')
+      .setLabel('Formato (MD1 ou MD3)')
+      .setStyle(TextInputStyle.Short)
+      .setRequired(true);
+
+    modal.addComponents(
+      new ActionRowBuilder().addComponents(time),
+      new ActionRowBuilder().addComponents(formato)
     );
+
+    await message.showModal(modal);
   }
 };
